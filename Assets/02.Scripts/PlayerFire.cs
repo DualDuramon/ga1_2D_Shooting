231bb9860ad1700716eeb66103a4649691eb9ca8
DualDura <jdsan216@gmail.com>
2026-09-02 14:40:54 +0900

    feat(player) : 총알 발사 쿨타임 적용

--- Assets/02.Scripts/PlayerFire.cs
+++ Assets/02.Scripts/PlayerFire.cs
@@ -9,11 +9,14 @@ public class PlayerFire : MonoBehaviour
     public GameObject BulletPrefab;
     //  - 생성위치(총구)
     public Transform[] MuzzleLocation;
+    public float FireCoolTime = 0.3f;
+
+    private float _lastFireTime = 0f;
 
     private void Update()
     {
         //1. 스페이스바를 누르면
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - _lastFireTime > FireCoolTime)
         {
             //2. 총알 발사
             FireBullet();
@@ -26,5 +29,7 @@ public class PlayerFire : MonoBehaviour
         {
             Instantiate(BulletPrefab, muzzleTf.position, Quaternion.identity);
         }
+
+        _lastFireTime = Time.time;
     }
 }
