8b1da9e55c59eabb17ef4158163e1a0f8eba9d1c
DualDura <jdsan216@gmail.com>
2026-09-10 13:15:15 +0900

    feat: 스코어매니저 및 점수 UI 생성
    - 스코어 매니저가 매 프레임마다 점수 표시

---
+++ Assets/02.Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,29 @@
+
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    //관리 : 특정 데이터에 대한 무결성과 읽기, 추가, 수정, 삭제 등과 관련된 로직들을 일컫는다.
+    private int _bestScore;
+    private int _currentScore;
+
+
+    //UI 책임 추가(TMP 참조)
+    [SerializeField] private TextMeshProUGUI _bestScoreText;
+    [SerializeField] private TextMeshProUGUI _currentScoreText;
+
+
+    private void Update()
+    {
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        _bestScoreText.text = $"BestScore : {_bestScore}";
+        _currentScoreText.text = $"Score : {_currentScore}";
+    }
+
+
+}
