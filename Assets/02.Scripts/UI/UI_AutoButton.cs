e62a4e406ee82629f961f745458aab29dbc1e84f
DualDura <jdsan216@gmail.com>
2026-09-14 11:11:21 +0900

    feat: buttonClick 애니메이션 추가
    - 버튼을 클릭할 때 버튼 애니메이션 재생되게 추가

--- Assets/02.Scripts/UI/UI_AutoButton.cs
+++ Assets/02.Scripts/UI/UI_AutoButton.cs
@@ -11,14 +11,30 @@ public class UI_AutoButton : MonoBehaviour
     [SerializeField] private Sprite _onSprite;
     [SerializeField] private Sprite _offSprite;
 
+    [Header("Button Click Feedback")]
+    [SerializeField] private AudioSource _audioSource;
+    private const float ButtonClickInterval = 0.3f;
+    private float _lastButtonClickTime = 0f;
+
+    [Header("Click Animation")]
+    [SerializeField] private Animator _animator;
+    [SerializeField] private AnimationCurve _bumpCurve;
 
     private Image _myImage;
     private bool _autoMode = false;
     private PlayerAutoMove _playerAutoMove;
 
+    private bool _isBumping = false;
+    private float _elapsedTime = 0f;
+    private const float BumpScale = 1.1f;
+    private const float BumpDuration = 0.3f;
+
+
     private void Awake()
     {
         _myImage = GetComponent<Image>(); //button의 이미지를 가져오게함
+        _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
         UpdateButtonImage();
     }
 
@@ -27,16 +43,59 @@ public class UI_AutoButton : MonoBehaviour
         _playerAutoMove = FindAnyObjectByType<PlayerAutoMove>();
     }
 
+    private void Update()
+    {
+        HandleBumpAnimation();
+    }
+
     public void AutoToggle()
     {
+        if (!CanActivateButton()) return;
+
         _autoMode = !_autoMode;
         _playerAutoMove.SetAutoMove(_autoMode);
 
         UpdateButtonImage();
+        PlayButtonFeedback();
+        _lastButtonClickTime = Time.time;
     }
 
     private void UpdateButtonImage()
     {
         _myImage.sprite = _autoMode ? _onSprite : _offSprite;
+    }
+
+    public void PlayButtonFeedback()
+    {
+        StartBumpAnimation();
+        _audioSource.Play();
+    }
+
+    private bool CanActivateButton()
+    {
+        return ButtonClickInterval < Time.time - _lastButtonClickTime;
+    }
+
+    private void HandleBumpAnimation()
+    {
+        if (!_isBumping) return;
+
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime > BumpDuration)
+        {
+            transform.localScale = Vector3.one;
+            _isBumping = false;
+            return;
+        }
+
+        float time = _elapsedTime / BumpDuration; //얼마나 지났는지 퍼센트(0~1);
+        float curveValue = _bumpCurve.Evaluate(time);
+        transform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * BumpScale, curveValue);
+    }
+
+    private void StartBumpAnimation()
+    {
+        _isBumping = true;
+        _elapsedTime = 0f;
     }
 }
